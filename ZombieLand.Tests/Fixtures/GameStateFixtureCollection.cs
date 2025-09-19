17237d66c7a1314d345601ed2ac8fdb83628efa4
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-09-19 15:02:38 -0300

    Trying to reproduce Fixture Issue

--- ZombieLand.Tests/Fixtures/GameStateFixtureCollection.cs
+++ ZombieLand.Tests/Fixtures/GameStateFixtureCollection.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ZombieLand.Tests.Fixtures
 {
-    [CollectionDefinition(GameStateFixtureCollection.CollectionName)]
-    public class GameStateFixtureCollection : ICollectionFixture<GameStateFixture>
-    {
-        public const string CollectionName = "Fixture Collection";
-    }
+    //[CollectionDefinition(GameStateFixtureCollection.CollectionName)]
+    //public class GameStateFixtureCollection : ICollectionFixture<GameStateFixture>
+    //{
+    //    public const string CollectionName = "Fixture Collection";
+    //}
 }
